830c35c67121b65cda01731124482376bb8ffcda
nebesnayaa <144495357+nebesnayaa@users.noreply.github.com>
2024-01-03 02:08:29 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HW17_file_editor
+{
+    public partial class MainWindow : Window
+    {
+        string mode = "unregistered";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CommandBinding binding;
+
+            binding = new CommandBinding(ApplicationCommands.New);
+            binding.Executed += NewCommand_Executed;
+            binding.CanExecute += FileCommands_CanExecute;
+
+            CommandBindings.Add(binding);
+
+            binding = new CommandBinding(ApplicationCommands.Open);
+            binding.Executed += OpenCommand_Executed;
+            binding.CanExecute += FileCommands_CanExecute;
+
+            CommandBindings.Add(binding);
+
+            binding = new CommandBinding(ApplicationCommands.Save);
+            binding.Executed += SaveCommand_Executed;
+            binding.CanExecute += FileCommands_CanExecute;
+            CommandBindings.Add(binding);
+
+
+            binding = new CommandBinding(ApplicationCommands.Close);
+            binding.Executed += CloseCommand_Executed;
+            CommandBindings.Add(binding);
+
+            binding = new CommandBinding(ApplicationCommands.Cut);
+            binding.CanExecute += EditCommands_CanExecute;
+            CommandBindings.Add(binding);
+
+            binding = new CommandBinding(ApplicationCommands.Copy);
+            binding.CanExecute += EditCommands_CanExecute;
+            CommandBindings.Add(binding);
+
+            binding = new CommandBinding(ApplicationCommands.Paste);
+            binding.CanExecute += EditCommands_CanExecute;
+            CommandBindings.Add(binding);
+        }
+
+        /* File */
+        private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            textField.Text = "";
+        }
+
+        private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textField.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textField.Text);
+            }
+        }
+
+        private void CloseCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void FileCommands_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (mode == "trial" || mode == "pro")
+            {
+                e.CanExecute = true;
+            }
+        }
+        /* \File */
+
+        private void EditCommands_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (mode == "pro")
+            {
+                e.CanExecute = true;
+            }
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
+
+        private void RegisterCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Registration registrationWindow = new Registration();
+            if (DialogResult == registrationWindow.ShowDialog())
+            {
+                mode = registrationWindow.accessMode;
+            }
+            mode = registrationWindow.accessMode;
+        }
+
+        private void textBox_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if ((e.Command == ApplicationCommands.Copy ||
+                 e.Command == ApplicationCommands.Cut ||
+                 e.Command == ApplicationCommands.Paste) && mode != "pro")
+            {
+                e.Handled = true;
+            }
+        }
+    }
+
+    public class DataCommands
+    {
+        private static RoutedUICommand Register;
+        static DataCommands()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.R, ModifierKeys.Control, "Ctrl+R"));
+            Register = new RoutedUICommand("Register", "Register", typeof(DataCommands), inputs);
+        }
+        public static RoutedUICommand _Register
+        {
+            get { return Register; }
+        }
+    }
+}
---
+++ Registration.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace HW17_file_editor
+{
+    public partial class Registration : Window
+    {
+        public string accessMode = "";
+
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        private void accessModeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (accessModeTextBox.Text.Length >= 3)
+            {
+                registerButton.IsEnabled = true;
+            }
+            else
+            {
+                registerButton.IsEnabled = false;
+            }
+        }
+        private void registerButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (accessModeTextBox.Text == "pro" || accessModeTextBox.Text == "trial")
+            {
+                accessMode = accessModeTextBox.Text;
+                DialogResult = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong key!");
+            }
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
